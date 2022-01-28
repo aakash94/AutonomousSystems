46eee6ddac72e55faf5f8409b102f1a42879cfff
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-01-28 13:34:50 +0100

    Fix cgi error

--- Lab1/grading.py
+++ Lab1/grading.py
@@ -15,6 +15,7 @@
 "Common code for autograders"
 
 import cgi
+import html
 import time
 import sys
 import json
@@ -291,7 +292,10 @@ to follow your instructor's guidelines to receive credit on your project.
         if self.mute: util.unmutePrint()
         print('*** ' + message)
         if self.mute: util.mutePrint()
-        message = cgi.escape(message)
+        # message = cgi.escape(message)
+        # Above line commented in favour of the line below, to fix error.
+        # Refer https://stackoverflow.com/a/62471248
+        message = html.escape(message)
     self.messages[self.currentQuestion].append(message)
 
   def addMessageToEmail(self, message):
